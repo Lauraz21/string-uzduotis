ac001cebbf5a062b26b6dfe86352f1ac7c06ea6e
Laura <Laura@DESKTOP-BESSB6E>
2023-08-23 19:46:40 +0300

    Add project files.

---
+++ string uzduotis/Program.cs
@@ -0,0 +1,9 @@
+﻿string word = "Laba diena";
+Console.WriteLine(word.Substring(5));
+Console.WriteLine(word.Substring(0,5));
+Console.WriteLine(word.Replace('n','t'));
+Console.WriteLine(word.IndexOf("d"));
+word = " Labas ";
+Console.WriteLine(word.Trim());
+Console.WriteLine(word.ToLowerInvariant());
+Console.WriteLine(word.ToUpperInvariant());
